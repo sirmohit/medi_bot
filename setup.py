9f77027b0019945fb62cf4798062676185cc6853
Mohit Bhade <mohitbhade236@gmail.com>
2024-10-19 10:06:03 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Medical Chat Bot",
+    version = "0.0.0",
+    author = "Mohit Bhade",
+    author_email="mohitbhade236@gmail.com",
+    packages = find_packages(),
+    install_requirs = []
+
+)
